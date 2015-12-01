90d854b4974c5e65bae29ae76991b21827ded0e2
Dylan Tran <dylan.ad.tran@gmail.com>
2015-12-01 01:14:18 +0000

    fixed edit function not sending correct info to database

--- js/server.js
+++ js/server.js
@@ -7,7 +7,6 @@ function serverPush() {
 			url: "../cover/toEmail.php",
 			data: {email: email},
 			success: function (data) {
-				console.log(data);
 				$("#email").attr("placeholder", "Email received, thanks!");
 			}
 		});
@@ -18,45 +17,18 @@ function serverPush() {
 		document.getElementById('email').value = '';
 		$("#email").attr("placeholder", "Please try again.");
 	}
-
 }
 
-function createNewUser () {
-        var name = document.getElementById('userName').value;
-        var email = document.getElementById('inputEmail').value;
-        var pass1 = document.getElementById('inputPassword').value;
-        var pass2 = document.getElementById('confirmPassword').value;
-        if (pass1 != pass2) {
-         alert('passwords don\'t match!');
-         return;
-        }
-        $.ajax({
-            method: 'post',
-            url: 'mysql/testadd.php',
-            data: {
-            	name: name,
-            	email: email,
-            	pass: pass1
-            },
-            success: function() {
-            	console.log('success!');
-            }
-        });
-}
-function login() {
-	var email = document.getElementById('inputEmail');
-	var pass = document.getElementById('inputPassword');
-		$.ajax({
-			method: 'post',
-			url: '../php/login.php',
-			data: {
-				email: email,
-				password: pass
-			},
-		success: function(data) {
+function edit_about_me() {
+	var about_me = $('#about_me').text();
+	var update = window.prompt('Update your about me', about_me);
+	$.ajax({
+		method: 'post',
+		url: "../dashboard/about_me_update.php",
+		data: {about_me: update},
+		success: function (data) {
 			console.log(data);
 		}
-	    });
+	});
 }
-
 
